75289aff6970e7e25379f6fab342c321d1866307
Onah Bernard Chizoba <onahbernardchizoba@gmail.com>
2023-10-01 13:12:48 +0100

    created endpoints to post and get videos

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import multer from 'multer';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.static('public'));
+
+app.use(cors());
 
 const port = 5000;
 
